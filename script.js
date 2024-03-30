e834b4ce0a951bb469160f04218fc6adfdf99fc8
YuanMo2024 <165105614+YuanMo2024@users.noreply.github.com>
2024-03-30 10:44:54 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -6,6 +6,101 @@ if (window.innerWidth <= 600) {
   document.getElementById("menu").style.display = "none";
 }
 
+// 阿晴悬浮球
+let downtime = 0;
+let AqingDown = false;
+let AqingDrag = false;
+let AqingMovein = false;
+let isMobile = false;
+let setPosTime = 0;
+let longdown = null;
+let mousepos = {
+  x: 0,
+  y: 0,
+};
+let AqingOffset = {
+  x: 0,
+  y: 0,
+};
+
+const BG = document.getElementById("bg");
+const AQ = document.getElementById("Aqing");
+
+BG.addEventListener("touchstart", function () {
+  isMobile = true;
+});
+BG.addEventListener("touchend", function () {
+  isMobile = false;
+});
+
+BG.onmousemove = function (event) {
+  event = event || window.event;
+  if (AqingDrag) {
+    mousepos.x = event.clientX;
+    mousepos.y = event.clientY;
+    // window.console.log("mousepos = " + mousepos.x + "," + mousepos.y);
+    AQ.style.left = mousepos.x - AqingOffset.x + "px";
+    AQ.style.top = mousepos.y - AqingOffset.y + "px";
+  }
+  AQ.onmousemove = function () {
+    if (AqingDown) {
+      AQ.style.scale = 0.9;
+    } else {
+      AQ.style.scale = 1.1;
+    }
+  };
+  AQ.onmouseout = function () {
+    AQ.style.scale = 1;
+    if (AqingDown) {
+      clearTimeout(longdown);
+    }
+    if (!AqingDrag && AqingDown) {
+      AqingDown = false;
+      window.console.log(AqingDown);
+    }
+  };
+  AQ.onmousedown = function () {
+    AqingDown = true;
+    window.console.log(AqingDown);
+    downtime = Date.now();
+    longdown = setTimeout(function () {
+      if (AqingDown) {
+        AqingDrag = true;
+        window.console.log("长按");
+      }
+    }, 800);
+    AqingOffset.x = event.clientX - AQ.offsetLeft;
+    AqingOffset.y = event.clientY - AQ.offsetTop;
+    AQ.style.scale = 0.9;
+  };
+  AQ.onmouseup = function () {
+    if (isMobile) {
+      AQ.style.scale = 1;
+    } else {
+      AQ.style.scale = 1.1;
+    }
+    if (AqingDrag) {
+      AqingDrag = false;
+    } else if (AqingDown) {
+      window.console.log(countYou);
+      AqingClick();
+    }
+    AqingDown = false;
+    clearTimeout(longdown);
+  };
+};
+function AqingClick() {
+  document.getElementById("star").style.display = "none";
+  countYou++;
+  divYouxuan =
+    '<div id="divAqing' +
+    countYou +
+    '" class="neirong shedow">阿晴头像菜单测试' +
+    countYou +
+    "</div>";
+  document.getElementById("zhuti").insertAdjacentHTML("beforeend", divYouxuan);
+}
+
 //按钮高亮
 function Lighton(Id) {
   document.getElementById(Id).style.backgroundColor = "#81d4fa";
@@ -26,12 +121,14 @@ function F0() {
   window.open(
     "https://bbs.mihoyo.com/ys/obc/?bbs_presentation_style=no_header&utm_source=bbs&utm_medium=mys&utm_campaign=pcbox"
   );
+  document.getElementById("menu0").style.backgroundColor = "#b3e5fc";
 }
 
 //隐藏菜单按钮
 function F1() {
   meniIsOn = false;
   document.getElementById("menu").style.display = "none";
+  document.getElementById("menu1").style.backgroundColor = "#b3e5fc";
 }
 
 //优选方案模板div，模板创建计数countYou
@@ -50,6 +147,7 @@ function F2() {
     "</div>";
   window.console.log(countYou);
   document.getElementById("zhuti").insertAdjacentHTML("beforeend", divYouxuan);
+  document.getElementById("menu2").style.backgroundColor = "#b3e5fc";
 }
 
 //自选方案模板div，模板创建计数countYou
@@ -68,16 +166,23 @@ function F3() {
     "</div>";
   window.console.log(countZi);
   document.getElementById("zhuti").insertAdjacentHTML("beforeend", divZixuan);
+  document.getElementById("menu3").style.backgroundColor = "#b3e5fc";
 }
 
 // 录入角色
-function F4() {}
+function F4() {
+  document.getElementById("menu4").style.backgroundColor = "#b3e5fc";
+}
 
 //录入武器
-function F5() {}
+function F5() {
+  document.getElementById("menu5").style.backgroundColor = "#b3e5fc";
+}
 
 // 录入圣遗物
-function F6() {}
+function F6() {
+  document.getElementById("menu6").style.backgroundColor = "#b3e5fc";
+}
 
 //初始界面控制菜单栏
 function menuctl() {
@@ -88,67 +193,4 @@ function menuctl() {
     meniIsOn = false;
     document.getElementById("menu").style.display = "none";
   }
-}
-
-// 阿晴悬浮球
-let downtime = 0;
-let AqingDown = false;
-let setPosTime = 0;
-let mousepos = {
-  x: 0,
-  y: 0,
-};
-let AqingOffset = {
-  x: 0,
-  y: 0,
-};
-
-const BG = document.getElementById("bg");
-const AQ = document.getElementById("Aqing");
-
-BG.onmousemove = function (event) {
-  // event = event || window.event;
-  if (AqingDown && Date.now() >= downtime + 500) {
-    mousepos.x = event.clientX;
-    mousepos.y = event.clientY;
-    // window.console.log("mousepos = " + mousepos.x + "," + mousepos.y);
-    AQ.style.left = mousepos.x - AqingOffset.x + "px";
-    AQ.style.top = mousepos.y - AqingOffset.y + "px";
-  }
-  AQ.onmousemove = function () {
-    if (AqingDown) {
-      AQ.style.scale = 1;
-    } else {
-      AQ.style.scale = 1.1;
-    }
-  };
-  AQ.onmouseout = function () {
-    AQ.style.scale = 1;
-  };
-  AQ.onmousedown = function () {
-    AqingDown = true;
-    downtime = Date.now();
-    AqingOffset.x = event.clientX - AQ.offsetLeft;
-    AqingOffset.y = event.clientY - AQ.offsetTop;
-    AQ.style.scale = 1;
-  };
-  AQ.onmouseup = function () {
-    AQ.style.scale = 1.1;
-    if (AqingDown && Date.now() < downtime + 500) {
-      window.console.log(1);
-      AqingClick();
-    }
-    AqingDown = false;
-  };
-};
-function AqingClick() {
-  document.getElementById("star").style.display = "none";
-  countYou++;
-  divYouxuan =
-    '<div id="divAqing' +
-    countYou +
-    '" class="neirong shedow">阿晴头像菜单测试' +
-    countYou +
-    "</div>";
-  document.getElementById("zhuti").insertAdjacentHTML("beforeend", divYouxuan);
 }
